380424f761b0b6297bff8e329878f9338cdcc451
wassup-999 <sebasquispeperez@gmail.com>
2026-04-22 20:37:19 -0500

    Update UIInventory.cs

--- Assets/Scripts/Laboratorio 6/UIInventory.cs
+++ Assets/Scripts/Laboratorio 6/UIInventory.cs
@@ -1,11 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class UIInventory : MonoBehaviour
 {
     public InvetoryData inventoryData;
+    public TextMeshProUGUI LifeTxt;   
+    public TextMeshProUGUI SanityTxt;
+    public TextMeshProUGUI StaminaTxt;
+    public TextMeshProUGUI NumberItemTxt;
+
     void Start()
     {
-        
+        LifeTxt.text = "Life : " + inventoryData.LifePorcentage;
+        SanityTxt.text = "Sanity : " + inventoryData.SanityPorcentage;
+        StaminaTxt.text = "Stamina : " + inventoryData.StaminaPorcentage;
+        NumberItemTxt.text = "Items : " + inventoryData.NumberOfItems;
     }
 
     // Update is called once per frame
@@ -13,4 +22,6 @@ public class UIInventory : MonoBehaviour
     {
         
     }
+
+
 }
